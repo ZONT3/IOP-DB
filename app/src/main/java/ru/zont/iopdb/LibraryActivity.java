ee4acf602d6563b0166f6837a435786819455f30
zxc <zxc>
2021-09-11 15:46:03 +0300

    Library WIP

--- app/src/main/java/ru/zont/iopdb/LibraryActivity.java
+++ app/src/main/java/ru/zont/iopdb/LibraryActivity.java
@@ -1,28 +1,47 @@
 package ru.zont.iopdb;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.util.Objects;
 
 import ru.zont.iopdb.data.DataEntity;
+import ru.zont.iopdb.databinding.ActivityLibraryBinding;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    public static final String FRAGMENT_TAG = "lib_fragment";
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library);
+        ActivityLibraryBinding binding = ActivityLibraryBinding
+                .inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        setSupportActionBar(binding.libToolbar);
+        Objects.requireNonNull(getSupportActionBar())
+                .setDisplayHomeAsUpEnabled(true);
+
+        FragmentManager manager = getSupportFragmentManager();
         if (savedInstanceState == null) {
             final TDollListFragment fragment = new TDollListFragment();
             fragment.setOnItemClick(this::onClickEntity);
-            getSupportFragmentManager().beginTransaction()
+            manager.beginTransaction()
                     .setReorderingAllowed(true)
-                    .replace(R.id.lib_root, fragment)
+                    .replace(R.id.lib_root, fragment, FRAGMENT_TAG)
                     .commit();
         }
+
+        fragment = manager.findFragmentByTag(FRAGMENT_TAG);
     }
 
     private void onClickEntity(DataEntity entity, TDollAdapter.ViewHolder holder) {
@@ -34,5 +53,18 @@ public class LibraryActivity extends AppCompatActivity {
                 .replace(R.id.lib_root, TDollInfoFragment.class, args)
                 .addToBackStack(null)
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_library, menu);
+        MenuItem item = menu.findItem(R.id.lib_act_search);
+        setupSearchView((SearchView) item.getActionView());
+        return true;
+    }
+
+    private void setupSearchView(SearchView view) {
+        // TODO
     }
 }
